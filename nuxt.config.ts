b0b3fb763fa2f4c63aa86915d4fd1c03d4e6f9be
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-11 21:52:02 +0400

    created eslint configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
