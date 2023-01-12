912ed5fa32ed92cf85785368ed398df1c3919e60
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-12 22:44:59 +0400

    added nuxt layout

--- app.vue
+++ app.vue
@@ -1,24 +1,5 @@
 <template>
   <main>
-    <TheHeader />
-    <NuxtPage />
-    <h1>Below</h1>
-    <div
-      class="bg-green-300"
-    >
-      hello there
-    </div>
-    <div class="text-primary">
-      {{ isDark }}
-    </div>
-    <TheFooter />
+    <NuxtLayout name="default" />
   </main>
 </template>
-
-<script lang="ts" setup>
-import { appStore } from '@/stores/app'
-const store = appStore()
-const isDark = computed(function (): boolean {
-  return store.darkMode
-})
-</script>
