6b6a63b67f1197b391561a5afb21290bb1284edf
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-14 18:02:49 +0400

    added links

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,24 @@
 <template>
   <header class="text-white">
     <div class="h-full container mx-auto flex flex-row justify-between">
-      <div class="my-auto">logo here</div>
+      <div class="my-auto">luka</div>
       <div>
         <nav class="h-full">
-          <ul class="flex flex-row">
-            <li class="">
-              <NuxtLink to="/"> Home </NuxtLink>
+          <ul class="flex flex-row h-full">
+            <li
+              class="my-auto mx-2 text-sm transition-colors text-zinc-400 hover:text-gray-200"
+            >
+              <NuxtLink to="/docs"> Docs </NuxtLink>
             </li>
-            <li class="">
-              <NuxtLink to="/about"> Home </NuxtLink>
+            <li
+              class="my-auto mx-2 text-sm transition-colors text-zinc-400 hover:text-gray-200"
+            >
+              <NuxtLink to="/roadmap"> Roadmap </NuxtLink>
+            </li>
+            <li
+              class="my-auto mx-2 text-sm transition-colors text-zinc-400 hover:text-gray-200"
+            >
+              <NuxtLink to="/support"> Support </NuxtLink>
             </li>
           </ul>
         </nav>
@@ -18,12 +27,16 @@
         <ul class="h-full flex flex-row">
           <li class="flex flex-col justify-center">
             <a href="/">
-              <i class="mdi mdi-linkedin mdi-24px mr-2"></i>
+              <i
+                class="mdi mdi-linkedin mdi-24px mr-2 transition-colors text-zinc-400 hover:text-gray-200"
+              ></i>
             </a>
           </li>
           <li class="flex flex-col justify-center">
             <a href="/">
-              <i class="mdi mdi-github mdi-24px"></i>
+              <i
+                class="mdi mdi-github mdi-24px transition-colors text-zinc-400 hover:text-gray-200"
+              ></i>
             </a>
           </li>
         </ul>
