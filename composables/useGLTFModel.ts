ac8a8e60369cdc6f06bfe113eb383e47880f2461
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 22:07:07 +0400

    added composables for gltf loader

---
+++ composables/useGLTFModel.ts
@@ -0,0 +1,14 @@
+ // add gltf model
+  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+ export function useGLTFModel(){
+    const gltfLoader = new GLTFLoader();
+    function load (url: string): unknown{
+        return new Promise((resolve, reject)=>{
+            gltfLoader.load(url, resolve, undefined, reject)
+        })
+    }
+    return {
+        load
+    }
+ }
