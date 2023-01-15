fa71432d04e27b8e8e406942a0337478f1c6db15
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 13:43:12 +0400

    removed dummy data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,5 @@
 <template>
-    <TheHeader />
-    <NuxtPage />
-    <h1>Below</h1>
-    <div
-      class="bg-green-300"
-    >
-      hello there
-    </div>
-    <div class="text-primary">
-      {{ isDark }}
-    </div>
-    <TheFooter />
+  <TheHeader />
+  <NuxtPage class="pt-16" />
+  <TheFooter />
 </template>
-
-
-<script lang="ts" setup>
-import { appStore } from '@/stores/app';
-const store = appStore()
-const isDark = computed(function (): boolean {
-  return store.darkMode
-})
-</script>
