815d6663d7f8bb3ff3f1bd18d743576913478111
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 16:12:11 +0400

    added links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,17 +1,45 @@
 <template>
-  <footer class="border-t-2 border-gray-100  text-white">
-    <div>
-      <a href="/" rel="noopener" target="_blank">
-        <p class="text-sm font-semibold">Made with N</p>
-      </a> 
+  <footer class="border-t-[1px] bg-black border-white/20 text-white">
+    <div class="container mx-auto flex flex-row justify-between h-full px-4">
+      <div class="flex items-center">
+        <p class="text-sm">
+          Made with
+          <a href="https://nuxt.com/" target="_blank">
+            <i class="mdi mdi-nuxt text-green-600"></i>
+          </a>
+          <a href="https://tailwindcss.com/" target="_blank">
+            <i class="mdi mdi-tailwind text-blue-500"></i>
+          </a>
+        </p>
+      </div>
+      <div class="flex items-center">
+        <ul class="flex flex-row">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/luka-jioshvili/"
+              target="_blank"
+            >
+              <i
+                class="mdi mdi-linkedin mdi-24px mr-2 transition-colors text-zinc-400 hover:text-gray-200"
+              ></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/LukaJioshvili20" target="_blank">
+              <i
+                class="mdi mdi-github mdi-24px mr-2 transition-colors text-zinc-400 hover:text-gray-200"
+              ></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-footer{
-  background-color: #0c0c0df2;
-  max-height: 64px;
-  height: 64px;
-}
+  footer {
+    max-height: 64px;
+    height: 64px;
+  }
 </style>
