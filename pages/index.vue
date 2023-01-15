1e4c6c2b2f358be2884ca7afd51a61e7a834315b
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 13:43:21 +0400

    added meta example

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,31 @@
 <template>
   <h1>Index page</h1>
 </template>
+
+<script lang="ts" setup>
+  useHead({
+    title: "Luka Jioshvili - Homepage",
+    meta: [
+      {
+        hid: "title",
+        name: "title",
+        content: "Luka Jioshvili | Homepage",
+      },
+      {
+        hid: "author",
+        name: "author",
+        content: "Luka Jioshvili",
+      },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content: "keywords for index page",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "description for index page",
+      },
+    ],
+  });
+</script>
