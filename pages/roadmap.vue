05023aded20b1052d3f2e0fdade2c17f2424cc6b
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 22:07:19 +0400

    added composables

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -7,33 +7,31 @@
 
 <script setup lang="ts">
   import { useWindowSize } from "@vueuse/core";
-  import {
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    SphereGeometry,
-    WebGLRenderer,
-  } from "three";
+  import { AmbientLight, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+  import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
   // setup
   let renderer: WebGLRenderer;
+  let controls: OrbitControls;
   const { width, height } = useWindowSize();
   const aspectRatio = computed(() => width.value / height.value);
 
+  // const bgColor = new Color("#E1F0C2");
   const scene = new Scene();
+  // scene.fog = new Fog(bgColor, 0.1, 75);
   const exeperiance = ref<HTMLCanvasElement | null>(null);
   // added camera
   const camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000);
   camera.position.set(0, 0, 5);
-
   scene.add(camera);
-  // added 3d object
-  const sphere = new Mesh(
-    new SphereGeometry(1, 32, 32),
-    new MeshBasicMaterial({ color: 0xff0000 })
-  );
-  scene.add(sphere);
 
+  const ambientLight = new AmbientLight(0xffffff, 0.8);
+  scene.add(ambientLight);
+
+  const { load } = useGLTFModel();
+  const { scene: model }: any = await load("/nuxty/nuxty.gltf");
+
+  scene.add(model);
   function updateRenderer() {
     renderer.setSize(width.value, height.value);
     renderer.render(scene, camera);
@@ -46,12 +44,18 @@
     if (exeperiance.value) {
       renderer = new WebGLRenderer({
         canvas: exeperiance.value,
+        alpha: true,
       });
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true;
       updateRenderer();
     }
   }
   function loop() {
-    sphere.position.x += 0.01;
+    model.rotation.y += 0.01;
+
+    controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
   }
@@ -62,6 +66,5 @@
   onMounted(() => {
     setRenderer();
     loop();
-    console.log("on Mounted");
   });
 </script>
