c0806228b1c53cb9dcb86eaba0249dded4c8436d
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-15 15:40:15 +0400

    added dark mode switcher component

---
+++ components/ButtonDarkMode.vue
@@ -0,0 +1,16 @@
+<template>
+  <i
+    @click.left="toggleDarkTheme"
+    class="cursor-pointer mdi mdi-24px transition-colors text-zinc-400 hover:text-gray-200"
+    :class="isDarkTheme ? 'mdi-lightbulb-on' : 'mdi-lightbulb'"
+  ></i>
+</template>
+
+<script lang="ts" setup>
+  import { AppActions, appStore } from "@/stores/app";
+  const store = appStore();
+  const isDarkTheme = computed(() => store.darkModeGetter);
+  function toggleDarkTheme() {
+    store[AppActions.themeToggleAction]();
+  }
+</script>
