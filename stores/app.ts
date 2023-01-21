988753d8c34e16b9a39728fc7a7d5962b8c1e99f
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-21 13:40:44 +0400

    updated theme toggle

--- stores/app.ts
+++ stores/app.ts
@@ -1,42 +1,54 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 interface AppState {
-  isMobile: boolean,
-  isNavigationOpened: boolean,
-  darkMode: boolean,
+  isMobile: boolean;
+  isNavigationOpened: boolean;
+  darkMode: boolean;
 }
 export enum AppActions {
-  themeToggleAction = 'themeToggleAction',
-  toggleNavigationAction = 'toggleNavigationAction',
-  isMobileAction = 'isMobileAction',
+  themeToggleAction = "themeToggleAction",
+  toggleNavigationAction = "toggleNavigationAction",
+  isMobileAction = "isMobileAction",
 }
 
-export const appStore = defineStore('app', {
+export const appStore = defineStore("app", {
   state: (): AppState => ({
     isMobile: false,
-    isNavigationOpened:false,
+    isNavigationOpened: false,
     darkMode: false,
   }),
   getters: {
-    darkModeGetter: (state):boolean=> {
-      return state.darkMode
+    darkModeGetter: (state): boolean => {
+      return state.darkMode;
     },
-    isNavigationOpenedGetter: (state):boolean=> {
-      return state.isNavigationOpened
+    isNavigationOpenedGetter: (state): boolean => {
+      return state.isNavigationOpened;
     },
-    isMobileGetter: (state):boolean => {
-      return state.isMobile
-    }
+    isMobileGetter: (state): boolean => {
+      return state.isMobile;
+    },
   },
   actions: {
-    [AppActions.themeToggleAction] ():void {
-      this.darkMode = !this.darkMode
+    [AppActions.themeToggleAction](payload?: boolean): void {
+      if (typeof payload === "boolean") {
+        this.darkMode = payload;
+      } else {
+        this.darkMode = !this.darkMode;
+      }
+
+      if (this.darkMode) {
+        localStorage.setItem("darkTheme", "true");
+        document.documentElement.classList.add("dark");
+      } else {
+        localStorage.setItem("darkTheme", "false");
+        document.documentElement.classList.remove("dark");
+      }
     },
-    [AppActions.toggleNavigationAction] (): void{
-      this.isNavigationOpened = !this.isNavigationOpened
+    [AppActions.toggleNavigationAction](): void {
+      this.isNavigationOpened = !this.isNavigationOpened;
     },
-    [AppActions.isMobileAction] (payload: boolean): void{
-      this.isMobile = payload
+    [AppActions.isMobileAction](payload: boolean): void {
+      this.isMobile = payload;
     },
-  }
-})
+  },
+});
