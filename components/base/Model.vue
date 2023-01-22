2a701d7f97e4befcbd8093b7c94731f1e4274325
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-22 20:05:42 +0400

    added size to canvas

--- components/base/Model.vue
+++ components/base/Model.vue
@@ -3,26 +3,39 @@
 </template>
 
 <script setup lang="ts">
-  import { useWindowSize } from "@vueuse/core";
+  import { PropType } from "vue";
+
   import { AmbientLight, PerspectiveCamera, Scene, WebGLRenderer } from "three";
   import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+  interface ModelObjectType {
+    width: number;
+    height: number;
+  }
   const props = defineProps({
     modelPath: {
       type: String,
       default: null,
     },
+    modelSize: {
+      type: Object as PropType<ModelObjectType>,
+      default: null,
+    },
   });
-
+  console.log(props.modelSize, "updates");
   // setup
   let renderer: WebGLRenderer;
   let controls: OrbitControls;
-  const { width, height } = useWindowSize();
-  const aspectRatio = computed(() => width.value / height.value);
+  const exeperiance = ref<HTMLCanvasElement | null>(null);
+
+  // const { width, height } = useWindowSize();
+
+  const aspectRatio = computed(
+    () => props.modelSize.width / props.modelSize.height
+  );
 
   // const bgColor = new Color("#E1F0C2");
   const scene = new Scene();
   // scene.fog = new Fog(bgColor, 0.1, 75);
-  const exeperiance = ref<HTMLCanvasElement | null>(null);
   // added camera
   const camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000);
   camera.position.set(0, 0, 5);
@@ -36,7 +49,7 @@
 
   scene.add(model);
   function updateRenderer() {
-    renderer.setSize(width.value, height.value);
+    renderer.setSize(props.modelSize.width, props.modelSize.height);
     renderer.render(scene, camera);
   }
   function updateCamera() {
