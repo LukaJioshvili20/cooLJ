05bd6fcd4217e1953719b7ddd3f0b2a378e4e31d
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-22 20:27:21 +0400

    update website icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app:{
-    head:{
+  app: {
+    head: {
       link: [
         {
-          href: 'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined',
-          rel: 'stylesheet',
+          href: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined",
+          rel: "stylesheet",
         },
         {
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
-          rel: 'stylesheet',
+          href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+          rel: "stylesheet",
         },
         {
-          href: '/apple-touch-icon.png',
-          rel: 'apple-touch-icon',
+          href: "/apple-touch-icon.png",
+          rel: "apple-touch-icon",
         },
         {
-          href: '/J-32.svg',
-          rel: 'icon',
-          type: 'image/x-icon'
+          href: "/J-32.svg",
+          rel: "icon",
+          type: "image/x-icon",
         },
+      ],
+      meta: [
         {
           hid: "og:site_name",
           name: "og:site_name",
@@ -51,24 +53,19 @@ export default defineNuxtConfig({
           content: "@LJioshvili",
         },
       ],
-    }
+    },
   },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  plugins: [
-    { src: '~/plugins/three.js', mode: 'client' }
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  plugins: [{ src: "~/plugins/three.js", mode: "client" }],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
+      autoprefixer: {},
+    },
   },
   typescript: {
-    strict: true
+    strict: true,
   },
-  css: [
-    '@/assets/css/main.scss',
-    '@mdi/font/css/materialdesignicons.min.css'
-  ],
-  pages: true
-})
+  css: ["@/assets/css/main.scss", "@mdi/font/css/materialdesignicons.min.css"],
+  pages: true,
+});
