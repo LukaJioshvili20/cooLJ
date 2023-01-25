0a8d66375ca8792978504dd8bc87b8cac1030c26
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-01-25 17:20:36 +0400

    added custom container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,16 @@ module.exports = {
         "grey-passive": "#A1A1AA",
         "backgroundDark": "#0c0c0f",
         "backgroundDark-2": "#27272a"
-      }
+      },
+      container: {
+        screens: {
+          sm: '600px',
+          md: '728px',
+          lg: '984px',
+          xl: '1024px',
+          '2xl': '1280px',
+        },
+      },
     }
   },
   plugins: []
