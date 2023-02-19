d8179f87c35b0fcbe2971ea3b652405fc3cde3c1
Luka Jioshvili <luka.jioshvili20@gmail.com>
2023-02-19 13:08:25 +0400

    added page for tech page content edit

---
+++ pages/admin/edit-tech.vue
@@ -0,0 +1,76 @@
+<template>
+  <main>
+    <section>
+      <div class="container mx-auto w-full items-center mb-8">
+        <ul
+          class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-zinc-600"
+        >
+          <li class="mr-2">
+            <a
+              @click="addTechA"
+              aria-current="page"
+              :class="addTech ? 'active bg-zinc-100' : 'bg-zinc-900'"
+              class="cursor-pointer inline-block p-4 text-red-400 font-bold rounded-t-lg transition-all"
+              >Add Tech</a
+            >
+          </li>
+          <li class="mr-2">
+            <a
+              @click="updateTechA"
+              :class="updateTech ? 'active bg-zinc-100' : 'bg-zinc-900'"
+              class="cursor-pointer inline-block p-4 rounded-t-lg text-red-400 bg-zinc-900 transition-all"
+              >Update Tech</a
+            >
+          </li>
+          <li class="mr-2">
+            <a
+              @click="deleteTechA"
+              :class="deleteTech ? 'active bg-zinc-100' : 'bg-zinc-900'"
+              class="cursor-pointer inline-block p-4 rounded-t-lg text-red-400 bg-zinc-900 transition-all"
+              >Delete Tech</a
+            >
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div class="container mx-auto">
+        <AdminAddTechItem v-if="addTech" />
+        <AdminUpdateTechItem v-if="updateTech" />
+        <AdminDeleteTechItem v-if="deleteTech" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup>
+  const user = useSupabaseUser();
+  const addTech = ref(true);
+  const updateTech = ref(false);
+  const deleteTech = ref(false);
+
+  function addTechA() {
+    addTech.value = true;
+    updateTech.value = false;
+    deleteTech.value = false;
+  }
+  function updateTechA() {
+    updateTech.value = true;
+    addTech.value = false;
+    deleteTech.value = false;
+  }
+  function deleteTechA() {
+    deleteTech.value = true;
+    addTech.value = false;
+    updateTech.value = false;
+  }
+  watchEffect(() => {
+    if (!user.value) {
+      return navigateTo("/login");
+    }
+  });
+  definePageMeta({
+    middleware: "auth",
+    layout: "admin",
+  });
+</script>
